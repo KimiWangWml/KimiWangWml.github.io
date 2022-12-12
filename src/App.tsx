41040661ac2d56a8ab445bd66b23fa2fe652e154
KimiWang <102603313+KimiWangWml@users.noreply.github.com>
2022-12-12 18:40:55 +0800

    formatted

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,10 @@ import { useState } from 'react'
 import { DatePicker } from 'antd'
 import reactLogo from './assets/react.svg'
 
-
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      Hello World!
-    </div>
-  )
+  return <div className="App">Hello World!</div>
 }
 
 export default App
