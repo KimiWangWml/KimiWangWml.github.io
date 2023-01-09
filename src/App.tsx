916b74d29499e08dc5449d1b027fbae90af4504a
David M. Chen <chen7david@me.com>
2023-01-09 10:21:20 +0800

    added base url

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
 import { DatePicker } from 'antd'
 import reactLogo from './assets/react.svg'
-import { Layout, Menu} from 'antd'
-const { Header, Footer, Content, Sider} = Layout
+import { Layout, Menu } from 'antd'
+const { Header, Footer, Content, Sider } = Layout
 
 function App() {
   const [count, setCount] = useState(0)
-
 
   const items = [
     { label: 'item 1', key: 'item-1' }, // remember to pass the key prop
@@ -15,18 +14,18 @@ function App() {
       label: 'sub menu',
       key: 'submenu',
     },
-  ];
+  ]
 
   return (
-      <Layout style={{ height: '100vh' }}>
-          <Header style={{ background: "white" }}>
-          <Menu mode="horizontal" items={items}></Menu>
-          </Header>
-          <Layout>
-            <Sider></Sider>
-            <Content></Content>
-          </Layout>
+    <Layout style={{ height: '100vh' }}>
+      <Header style={{ background: 'white' }}>
+        <Menu mode="horizontal" items={items}></Menu>
+      </Header>
+      <Layout>
+        <Sider></Sider>
+        <Content></Content>
       </Layout>
+    </Layout>
   )
 }
 
