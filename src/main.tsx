d26b98d7f2871ec764b41792cfab9ef534bef4a6
David M. Chen <chen7david@me.com>
2023-02-13 06:18:14 -0500

    added the router

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'antd/dist/antd.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/kimi',
+    element: <div>Hello kimi!</div>,
+  },
+  {
+    path: '/lele',
+    element: <div>Hello lele!</div>,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
