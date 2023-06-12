ef5dc4113a2a6d44e6bf7f1da106b2e7c1f0c771
KimiWang <102603313+KimiWangWml@users.noreply.github.com>
2023-06-12 18:23:15 +0800

    add things

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,20 @@
 import { useState } from 'react'
+import { DatePicker } from 'antd'
+import reactLogo from './assets/react.svg'
 import { Layout, Menu } from 'antd'
+import { RouterMap } from './Router'
+import { NavigationMenu } from './components/NavigationMenu'
 const { Header, Footer, Content, Sider } = Layout
-import { router } from './routes'
-import { RouterProvider } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const items = [
-    { label: 'item 1', key: 'item-1' }, // remember to pass the key prop
-    { label: 'item 2', key: 'item-2' }, // which is required
-    {
-      label: 'sub menu',
-      key: 'submenu',
-    },
-  ]
-
-  const onChange = (checked: boolean) => {
-    console.log(`switch to ${checked}`)
-  }
-
   return (
     <Layout style={{ height: '100vh' }}>
-      <Header style={{ background: 'white' }}>
-        <Menu mode="horizontal" items={items}></Menu>
-      </Header>
+      <NavigationMenu />
+
       <Layout>
+        <Sider></Sider>
         <Content>
-          <RouterProvider router={router} />
+          <RouterMap />
         </Content>
       </Layout>
     </Layout>
